43f4619bff3eb6704e7d6b9bfe0b3a7469a95e55
yantou <yantou_mu@163.com>
2022-08-09 23:26:07 +0800

    feat : 布局多选删除

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,12 @@
     </div>
     <!-- query -->
     <div class="query-box">
-      <el-input v-model="queryInput" placeholder="请输入姓名搜索" />
-      <el-button type="primary">增加</el-button>
+      <el-input class="query-input" v-model="queryInput" placeholder="请输入姓名搜索" />
+      <div class="btn-list">
+        <el-button type="primary" @click="handleAdd">增加</el-button>
+        <el-button type="danger" @click="handleDelList" v-if="multipleSelection.length>0">删除多选</el-button>
+      </div>
+
     </div>
     <!-- table -->
     <el-table
@@ -19,90 +23,162 @@
         style="width: 100%"
         @selection-change="handleSelectionChange"
         border>
+
+
       <el-table-column type="selection" width="55" />
-      <el-table-column fixed prop="date" label="Date" width="150" />
-      <el-table-column prop="name" label="Name" width="120" />
-      <el-table-column prop="state" label="State" width="120" />
-      <el-table-column prop="city" label="City" width="120" />
-      <el-table-column prop="address" label="Address" width="600" />
-      <el-table-column prop="zip" label="Zip" width="120" />
-      <el-table-column fixed="right" label="Operations" width="120">
-        <template #default>
-          <el-button link type="primary" size="small" @click="handleRowClick"
-          >Detail</el-button>
-          <el-button link type="primary" size="small">Edit</el-button>
+      <el-table-column prop="name" label="姓名" width="120" />
+      <el-table-column prop="email" label="邮箱" width="120" />
+      <el-table-column prop="phone" label="电话" width="120" />
+      <el-table-column prop="state" label="状态" width="120" />
+      <el-table-column prop="address" label="地址" width="600" />
+
+      <el-table-column fixed="right" label="操作" width="120">
+        <template #default="scope">
+          <el-button link type="primary" size="small" @click="handleRowDel(scope.row)" style="color: #F56C6C">删除</el-button>
+          <el-button link type="primary" size="small" style="color: aqua">编辑</el-button>
         </template>
       </el-table-column>
     </el-table>
+    <!-- dialog -->
+    <el-dialog v-model="dialogFormVisible" :title="dialogType=='add'? '新增': '编辑'">
+      <el-form :model="tableForm">
+        <el-form-item label="姓名" :label-width="60">
+          <el-input v-model="tableForm.name" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="邮箱" :label-width="60">
+          <el-input v-model="tableForm.email" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="电话" :label-width="60">
+          <el-input v-model="tableForm.phone" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="状态" :label-width="60">
+          <el-input v-model="tableForm.state" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="地址" :label-width="60">
+          <el-input v-model="tableForm.address" autocomplete="off" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+      <span class="dialog-footer">
+        <el-button type="primary" @click="dialogConfirm">
+          确定
+        </el-button>
+      </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 <script setup>
 
-import {ref} from "vue";
 /*  数据  */
-let queryInput =ref("")
-let tableData =ref([
+let queryInput =$ref("")
+let tableData =$ref([
   {
-    date: '2016-05-03',
+    id:"1",
     name: 'Tom',
+    email:"123@qq.com",
     state: 'California',
-    city: 'Los Angeles',
+    phone:"13629183123",
     address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Home',
   },
   {
-    date: '2016-05-02',
+    id:"2",
     name: 'Tom',
+    email:"123@qq.com",
     state: 'California',
-    city: 'Los Angeles',
+    phone:"13629183123",
     address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Office',
   },
   {
-    date: '2016-05-04',
+    id:"3",
     name: 'Tom',
+    email:"123@qq.com",
     state: 'California',
-    city: 'Los Angeles',
+    phone:"13629183123",
     address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Home',
   },
   {
-    date: '2016-05-01',
+    id:"4",
     name: 'Tom',
+    email:"123@qq.com",
     state: 'California',
-    city: 'Los Angeles',
+    phone:"13629183123",
     address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Office',
   },
-])
 
-let multipleSelection =  ref([])
+])
+let multipleSelection = $ref([])
+let dialogFormVisible = $ref(false)
+let tableForm = $ref({
+  name:"张三",
+  email:"123@qq.com",
+  phone:"13827183123",
+  state:"在职",
+  address:"北京"
+})
+let dialogType =$ref('add')
 
 /* 方法 */
-const handleRowClick =()=>{
-console.log("click")
+
+// 删除一条
+const handleRowDel=({id})=>{
+console.log(id)
+  // 通过id过去到条目对应的索引值
+  let  index =tableData.findIndex(item=>item.id===id)
+  // 通过索引值进行删除对应条目
+  tableData.splice(index,1)
 }
 
-const handleSelectionChange = (val) => {
-  multipleSelection.value = val
-  console.log(val);
+const handleDelList = ()=>{
+  multipleSelection.forEach(id=>{
+    handleRowDel({id})
+  })
+  multipleSelection = []
 }
+
+
+// 选中
+const handleSelectionChange = (val) => {
+  // multipleSelection = val
+  // console.log(val);
+  //选中之前清空
+  multipleSelection = []
+  val.forEach(item =>{
+    multipleSelection.push(item.id)
+  })
+}
+
+
+// 添加
+const handleAdd=()=>{
+  dialogFormVisible = true
+  //选中新增,清空数据
+  tableForm={}
+}
+
+// 确认
+const dialogConfirm=()=>{
+  //点击确认.关闭弹窗
+  dialogFormVisible=false
+  // 拿到数据
+  // 添加到 table
+  tableData.push({
+    id:(tableData.length+1).toString(),
+    ...tableForm
+  })
+  console.log(tableData);
+
+}
+
+
 </script>
 <style scoped>
 .table-box{
+  margin: 200px;
   width: 800px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform:translate(-50%,-50%);
 }
 .title{
 text-align: center;
-
 }
 .query-box{
 display: flex;
@@ -110,8 +186,10 @@ display: flex;
   margin-bottom: 20px;
 
 }
-.el-input{
-  width: 280px;
+.query-input{
+  width: 200px;
+
 }
 
 </style>
+
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+      /* 是否开启$ref vue3.2 的语法糖 */
+      vue({
+        refTransform:true,
+        reactivityTransform:true
+      }),
+  ],
+  server:{
+    port: 8000 // 指定端口
+  },
+  base: '/', //打包相对路径
 })
